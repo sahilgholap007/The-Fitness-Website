1da3cfd6e7e4db13f7f2d6dc1ef97b8d46bcaee4
sahilgholap007 <gholapsahil007@gmail.com>
2022-10-10 21:06:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Landing from "./pages/Landing"
+
+export default function App() {
+    return (
+        <div className="main">
+            <Landing/>
+        </div>
+    )
+}
