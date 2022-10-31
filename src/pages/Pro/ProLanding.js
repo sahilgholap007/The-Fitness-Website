7f3cd00339396168aec6cd848b49b82919922819
sahilgholap007 <gholapsahil007@gmail.com>
2022-10-31 20:38:21 +0530

    s

--- src/pages/Pro/ProLanding.js
+++ src/pages/Pro/ProLanding.js
@@ -26,7 +26,7 @@ export default function ProLanding() {
                     <a href='/Pro/Signin'>Log In</a>
                 </div>
                 <div className='text-alignment'>
-                    <h1>Know More</h1>
+                    <h1>Know Moree</h1>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the</p>
                 </div>    
                 <a id='pro-btn' href='/pro/ProLanding/Knowmore'>Discover</a>      
