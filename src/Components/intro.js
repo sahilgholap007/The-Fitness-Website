0df6b9fd24add195322d54df13aad576e94b4b6b
sahilgholap007 <gholapsahil007@gmail.com>
2022-11-03 21:54:44 +0530

    hehe

--- src/Components/intro.js
+++ src/Components/intro.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
 export default function Intro() {
+
+  function smoothScroll(){
+    document.querySelector('#exercise').scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+
   return (
     <section className='landing'>
         <div className='introduction'>
             <h1 id="heading">EXPERIENCE ALL<br/><span id='name'>TFW</span> HAS TO OFFER</h1>
             <p>WELCOME TO THE WORLD OF VIRTUAL FITNESS</p>
-            <div className='button' id='get-started'>
-                <li><a href='/'>Get started</a></li>
+            <div className='button' id='get-started'  onClick={smoothScroll}>
+                <li>
+                   {/* eslint-disable-next-line */}
+                  <a >Get Started</a>
+                </li>
             </div>
         </div>
     </section>
